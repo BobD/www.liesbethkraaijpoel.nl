7217ec2765bbf129734f75f6523e91a464dff7fb
Bob <b.donderwinkel@gmail.com>
2015-03-19 12:29:53 +0100

    several tweaks for CMS integration and faster loading

--- tasks/distribute.js
+++ tasks/distribute.js
@@ -7,12 +7,10 @@ module.exports = function(grunt) {
 
 		if(grunt.file.isDir(config.distribute.local)){
 			grunt.config('copy.distribute.files', [
-				{src: 'build/scripts/require.js', dest: endPath + '/scripts/require.js'},
-				{src: 'build/scripts/main.min.js', dest: endPath + '/scripts/main.min.js'},
 				{expand: true, cwd: 'dist/css/', src: ['**/*.*'], dest: endPath + '/css', filter: 'isFile'},
 				{expand: true, cwd: 'dist/images/assets', src: ['**/*.*'], dest: endPath + '/images/assets', filter: 'isFile'}
 			]);
-			grunt.task.run(['requirejs', 'copy:distribute']);
+			grunt.task.run(['default', 'copy:distribute']);
 		}
 
 	});
